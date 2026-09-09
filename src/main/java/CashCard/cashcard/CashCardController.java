fd1ec33ca6f3cb5ff883a1fe1ebc3fb1d06c1a05
Arrogz <108223317+Arrogz@users.noreply.github.com>
2026-09-09 23:03:16 +1000

    Get All

--- src/main/java/CashCard/cashcard/CashCardController.java
+++ src/main/java/CashCard/cashcard/CashCardController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
@@ -24,6 +28,16 @@ class CashCardController {
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         return cashCardRepository.findById(requestedId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
    }
+
+   @GetMapping
+   private ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+            PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSortOr(Sort.by("amount").ascending()))
+        );
+
+        return ResponseEntity.ok(page.getContent());
+   }
+
 
    @PostMapping
    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCard) {
